111c630e9a21c0988159ebf6251c503d87177dd5
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-05-29 21:38:45 -0700

    minor changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,10 +9,7 @@ function displaygifDiv(){
 
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=dc6zaTOxFJmzC&limit=10";
         
-    $.ajax({
-    	url: queryURL,
-    	method: "GET",
-    })
+    $.ajax({url: queryURL,method:'GET'})
     .done(function(response){
     	console.log(queryURL);
     	console.log(response);
